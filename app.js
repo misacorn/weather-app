e94db756e267f8a21dcadb1877dab31329819208
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-16 14:01:27 +0200

    parsing request bodies

--- app.js
+++ app.js
@@ -4,25 +4,37 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-  url === "/"
-    ? (res.write("<html>"),
-      res.write("<head><title>Enter Message</title></head>"),
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
-      ),
-      res.write("</html>"),
-      res.end())
-    : url === "/message" && method === "POST"
-    ? (fs.writeFileSync("message.txt", "DUMMY"),
-      (res.statusCode = 302),
-      res.setHeader("Location", "/"),
-      res.end())
-    : (res.setHeader("Content-type", "text/html"),
-      res.write("<html>"),
-      res.write("<head><title>First Page</title></head>"),
-      res.write("<body>Heluuuuuuu</body>"),
-      res.write("</html>"),
-      res.end());
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", chunk => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>First Page</title></head>");
+  res.write("<body>Heluuuuuuu</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(1000);
