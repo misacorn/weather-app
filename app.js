5f3199eaa442cf6bf669027f7d9ae636e35ae2b6
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2019-01-16 12:57:47 +0200

    routing request

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const http = require(); //how to import files in nodeJS
+const http = require("http"); //how to import files in nodeJS
 
 const server = http.createServer((req, res) => {
-  console.log(reg);
+  const url = req.url;
+  url === "/"
+    ? (res.write("<html>"),
+      res.write("<head><title>Enter Message</title></head>"),
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>'
+      ),
+      res.write("</html>"),
+      res.end())
+    : (res.setHeader("Content-type", "text/html"),
+      res.write("<html>"),
+      res.write("<head><title>Enter Message</title></head>"),
+      res.write("<bodyHeluuuuuuu</body>"),
+      res.write("</html>"),
+      res.end());
 });
 
-server.listen(3000);
+server.listen(1000);
