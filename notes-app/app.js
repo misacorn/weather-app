ca2d9733bcd8e1b77c05dc0d9f3c35d58070d1e7
Misa Corn <dieuhuongngo@gmail.com>
2019-03-18 23:36:08 +0200

    printing in color in console.log

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,16 @@
+const chalk = require("chalk");
+const log = console.log;
+
+log(chalk.bgRed("Success!"));
+
 // const fs = require("fs")
 
 // fs.writeFileSync("notes.txt", "This is the first line. ")
 // fs.appendFileSync("notes.txt", "And this is the second one.")
 
-const validator = require("validator");
-const text = require("./notes");
+// const validator = require("validator");
+// const text = require("./notes");
 
-const showText = text();
+// const showText = text();
 
-console.log(validator.isURL("gmail@co"));
+// console.log(validator.isURL("gmail@co"));
