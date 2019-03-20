6daab276b69cec9466a2a1b45b7add2ea1d0d0c2
Misa Corn <dieuhuongngo@gmail.com>
2019-03-20 13:00:02 +0200

    adding a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const text = require("./notes");
+const notes = require("./notes");
 
 // console.log(process.argv);
 
@@ -22,8 +22,7 @@ yargs.command({
     }
   },
   handler: function(argv) {
-    console.log("Title: " + argv.title);
-    console.log("Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   }
 });
 
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,44 @@
-const text = function () {
-  return "Your notes are ..."
-}
+const fs = require("fs");
 
-module.exports = text
+const getNotes = () => {
+  return "Your notes are ...";
+};
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(note => note.title === title);
+  // const duplicateNotes = notes.filter(function(note) {
+  //   return note.title === title;
+  // });
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
+
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  return fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = function() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes,
+  addNote
+};
