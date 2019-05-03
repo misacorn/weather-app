b9b5226af6d3b68e2137c15cb2cae2ac69b29d66
Misa <dieuhuongngo@gmail.com>
2019-05-04 00:06:15 +0300

    callback function challenge

--- playground/callbacks.js
+++ playground/callbacks.js
@@ -4,13 +4,33 @@ setTimeout(() => {
   console.log("Two seconds are up");
 }, 2000);
 
-//geoCode() does not return anything, JS implicitly returns undefined
+//geoCode() does not return anything, JS implicitly returns unde
+// const geoCode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = { lat: 0, long: 0 };
+//     return data;
+//   }, 2000);
+// };
+
+// const data = geoCode("Home");
+// console.log(data);
+
 const geoCode = (address, callback) => {
   setTimeout(() => {
     const data = { lat: 0, long: 0 };
-    return data;
+    callback(data);
   }, 2000);
 };
 
-const data = geoCode("Home");
-console.log(data);
+geoCode("Home", data => {
+  console.log(data);
+});
+
+const add = (a, b, c) => {
+  const sum = a + b;
+  c(sum);
+};
+
+add(1, 4, sum => {
+  console.log(sum); // Should print: 5
+});
