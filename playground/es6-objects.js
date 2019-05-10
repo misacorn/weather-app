05ca8312c506d2c0f08ad9d0e0e69fbd5a23a468
Misa <dieuhuongngo@gmail.com>
2019-05-10 22:18:54 +0300

    destructuring objects

---
+++ playground/es6-objects.js
@@ -0,0 +1,16 @@
+const product = {
+  label: "Red notebook",
+  price: "€30",
+  stock: 30
+};
+
+// const { label: productLabel, price, rating = "4/5" } = product;
+// console.log(productLabel);
+// console.log(price);
+// console.log(rating);
+
+const transaction = (type, { label, price }) => {
+  console.log(type + " " + label + " at the price of " + price);
+};
+
+transaction("Order", product);
