7402c8ea95d24a03376c7143e1f20d71173788c2
Misa <dieuhuongngo@gmail.com>
2019-05-23 22:01:44 +0300

    serving up w HTML & JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,24 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Helu Umimu");
+  res.send("<h1>Helu Umimu</h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Mimu");
+  res.send([
+    {
+      name: "Umimu",
+      age: "forever 18"
+    },
+    {
+      name: "Umicu",
+      age: "forever 81"
+    }
+  ]);
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Forecast");
+  res.send({ forcast: "Sunny", location: "Helsinki" });
 });
 
 app.listen(3000, () => {
