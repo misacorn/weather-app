dda9e9b86deff612c7bd2b7522e90845bc2f6255
Misa <dieuhuongngo@gmail.com>
2019-05-24 22:58:05 +0300

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,28 +7,49 @@ const express = require("express");
 const app = express();
 const pubD = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(pubD));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Mimu"
+  });
+});
 
 // app.get("", (req, res) => {
 //   res.send("<h1>Helu Umimu</h1>");
 // });
 
 app.get("/about", (req, res) => {
-  res.send([
-    {
-      name: "Umimu",
-      age: "forever 18"
-    },
-    {
-      name: "Umicu",
-      age: "forever 81"
-    }
-  ]);
+  res.render("about", {
+    title: "About"
+  });
 });
 
-app.get("/weather", (req, res) => {
-  res.send({ forcast: "Sunny", location: "Helsinki" });
+// app.get("/about", (req, res) => {
+//   res.send([
+//     {
+//       name: "Umimu",
+//       age: "forever 18"
+//     },
+//     {
+//       name: "Umicu",
+//       age: "forever 81"
+//     }
+//   ]);
+// });
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help!",
+    message: "Help me get a job!"
+  });
 });
+
+// app.get("/weather", (req, res) => {
+//   res.send({ forcast: "Sunny", location: "Helsinki" });
+// });
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
