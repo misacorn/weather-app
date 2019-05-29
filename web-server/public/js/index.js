1965a911fb5dfb33ada7123cc3203d9c74c479c8
Misa <dieuhuongngo@gmail.com>
2019-05-29 23:23:48 +0300

    search form

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,7 +1,12 @@
-fetch("http://localhost:3000/weather?search=helsinki").then(response => {
-  response.json().then(data => {
-    data.error && console.log(data.error);
-    console.log(data.location);
-    console.log(data.forecast);
+const weatherForm = document.querySelector("form");
+
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch("http://localhost:3000/weather?search=helsinki").then(response => {
+    response.json().then(data => {
+      data.error && console.log(data.error);
+      console.log(data.location);
+      console.log(data.forecast);
+    });
   });
 });
