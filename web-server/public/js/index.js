ad70f609dd802915c12b31e59076a6b6507a0879
Misa <dieuhuongngo@gmail.com>
2019-05-29 23:00:12 +0300

    fetching

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,1 +1,7 @@
-console.log("Helooooooo");
+fetch("http://localhost:3000/weather?search=helsinki").then(response => {
+  response.json().then(data => {
+    data.error && console.log(data.error);
+    console.log(data.location);
+    console.log(data.forecast);
+  });
+});
